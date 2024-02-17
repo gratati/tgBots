96626f35f3304cadae4e8291e7b631a600771224
Tatijana <gratati49@gmail.com>
2024-02-17 09:07:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,58 +5,74 @@ from openai import OpenAI
 
 # Инициализация клиента API OpenAI с вашим API ключом
 openai_client = OpenAI(
-    api_key="sk-qY1e6pDtlVsPTYdGvIU9cr5Je6WSjtqB",  # Замените на ваш API ключ
+    api_key="ваш API ключ",
     base_url="https://api.proxyapi.ru/openai/v1",
 )
 
-# Инициализация телеграм бота с вашим токеном
-TELEGRAM_BOT_TOKEN = '6292915409:AAGD2odL0Dl5H8ND7PjlrnooadiVmsdUo50'  # Замените на токен вашего бота
-bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
+# Установите свой токен от BotFather
+BOT_TOKEN = 'TELEGRAM_BOT_TOKEN '
+
+# Создаем экземпляр бота
+bot = telebot.TeleBot(BOT_TOKEN)
 
 # Словарь для хранения истории разговора с каждым пользователем
 conversation_histories = {}
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Привет! Я бот, который может поддержать разговор на любую тему.")
+    bot.reply_to(message, "Привет! Я бот, который может перевернуть текст, превратить его в верхний регистр, убрать гласные, "
+                          "подсчитать количество символов без пробелов и поддержать разговор на любую тему.")
+
+@bot.message_handler(commands=['palindrom'])
+def palindrom(message):
+    text = message.text[11:]
+    reversed_text = text[::-1]
+    bot.reply_to(message, f"Перевернутый текст: {reversed_text}")
+
+@bot.message_handler(commands=['caps'])
+def caps(message):
+    text = message.text[6:]
+    upper_text = text.upper()
+    bot.reply_to(message, f"Текст в верхнем регистре: {upper_text}")
+
+@bot.message_handler(commands=['letter'])
+def letter(message):
+    text = message.text[8:]
+    vowels = ['a', 'e', 'i', 'o', 'u', 'а', 'е', 'и', 'о', 'ю', 'у', 'я']
+    filtered_text = ''.join([letter for letter in text if letter.lower() not in vowels])
+    bot.reply_to(message, f"Текст без гласных букв: {filtered_text}")
+
+@bot.message_handler(commands=['calc'])
+def calc(message):
+    text = message.text[6:]
+    count = len(text.replace(" ", ""))
+    bot.reply_to(message, f"Количество символов без пробелов: {count}")
 
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
     user_id = message.chat.id
     user_input = message.text
 
-    # Если история для пользователя не существует, создаем новую
     if user_id not in conversation_histories:
         conversation_histories[user_id] = []
 
-    # Добавление ввода пользователя в историю разговора
     conversation_history = conversation_histories[user_id]
     conversation_history.append({"role": "user", "content": user_input})
 
-    # Отправка запроса в нейронную сеть
     chat_completion = openai_client.chat.completions.create(
         model="gpt-3.5-turbo-1106",
         messages=conversation_history
     )
 
-    # Извлечение и ответ на сообщение пользователя
     ai_response_content = chat_completion.choices[0].message.content
 
-    # Создаем объект gTTS для преобразования текста в речь
-    tts = gTTS(ai_response_content, lang='ru')  # Можете изменить язык на нужный вам
-    # Создаем временный файл для сохранения аудио
+    tts = gTTS(ai_response_content, lang='ru')
     temp_file = "temp_audio.mp3"
     tts.save(temp_file)
 
-    # Отправляем аудио-сообщение пользователю
     with open(temp_file, 'rb') as audio:
         bot.send_voice(message.chat.id, audio)
 
-    # Удаляем временный файл
     os.remove(temp_file)
 
-    # Добавление ответа нейронной сети в историю разговора
     conversation_history.append({"role": "system", "content": ai_response_content})
-
-if __name__ == '__main__':
-    bot.infinity_polling()
